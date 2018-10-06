8b633bb2aa82090073f440aa6cd1567d4999c116
Gazi Al-Amin <37772368+gazialamin96@users.noreply.github.com>
2018-10-06 09:59:43 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 
-int main() {
+int main()
+{
+    int num1,num2,num3;
+    printf("Enter the three numbers : ");
+    scanf("%d %d %d",&num1,&num2,&num3);
 
-    char operator;
-    double firstNumber,secondNumber;
-
-    printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operator);
-
-    printf("Enter two operands: ");
-    scanf("%lf %lf",&firstNumber, &secondNumber);
-
-    switch(operator)
-    {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf",firstNumber, secondNumber, firstNumber+secondNumber);
-            break;
-
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf",firstNumber, secondNumber, firstNumber-secondNumber);
-            break;
-
-        case '*':
-            printf("%.1lf * %.1lf = %.1lf",firstNumber, secondNumber, firstNumber*secondNumber);
-            break;
-
-        case '/':
-            printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, firstNumber/secondNumber);
-            break;
-
-        // operator is doesn't match any case constant (+, -, *, /)
-        default:
-            printf("Error! operator is not correct");
+    if(num1>num2 && num1>num3){
+            printf("%d is bigger \n",num1);
+            if(num2>num3){
+                printf("%d is middle \n %d is smaller ",num2,num3);
+            }
+            else{
+                printf("number %d is middle\n %d is smaller",num3,num2);
+            }
     }
+
+
+    if(num2>num1 && num2>num3){
+            printf(" %d is bigger \n",num2);
+            if(num1>num3){
+                printf("%d is middle\n %d is smaller",num1,num3);
+            }
+            else{
+                printf(" %d is middle\n %d is smaller",num3,num1);
+            }
+    }
+
+
+    if(num3>num1 && num3>num2){
+            printf("%d is bigger\n",num3);
+            if(num1>num2){
+                printf("%d is middle \n",num1);
+            }
+            else{
+                printf("%d is middle \n %d is smaller\n",num2,num1);
+            }
+    }
+
+    getch();
 
     return 0;
 }
